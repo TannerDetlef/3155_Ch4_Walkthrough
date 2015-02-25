f148c98d590b07e667a78ac03eb54502a1ce301d
saasbook <saasbook@saasbook.(none)>
2015-02-24 20:34:35 -0400

    Section 4.7

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,6 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
-end
 
 # in app/controllers/movies_controller.rb
  
@@ -17,3 +16,11 @@ def new
   # default: render 'new' template
 end
 
+# in movies_controller.rb
+def create
+  @movie = Movie.create!(params[:movie])
+  flash[:notice] = "#{@movie.title} was successfully created."
+  flash[:warning] = "There was an error adding #{@movie.title}!"
+  redirect_to movies_path
+end
+end
